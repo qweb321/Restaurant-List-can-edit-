12ff315985e78eac9c6f7b5fc82d0e6bf4beda1d
Isis Lin <qweb32114@gmail.com>
2022-11-09 13:24:11 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const ResList = require("./model/res-list");
 const port = 3000;
 const app = express();
@@ -24,6 +25,8 @@ db.once("open", () => {
 app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: "hbs" }));
 app.set("view engine", "hbs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   ResList.find()
     .lean()
@@ -31,11 +34,38 @@ app.get("/", (req, res) => {
     .catch((error) => console.log(error));
 });
 
+// detail
 app.get("/restaurants/:id", (req, res) => {
   const id = req.params.id;
   return ResList.findById(id)
     .lean()
     .then((restaurant) => res.render("detail", { restaurant }))
+    .catch((error) => console.log(error));
+});
+
+// edit
+app.get("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
+  return ResList.findById(id)
+    .lean()
+    .then((restaurant) => res.render("edit", { restaurant }))
+    .catch((error) => console.log(error));
+});
+
+app.post("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const edit = req.body;
+  return ResList.findById(id)
+    .then((restaurant) => {
+      restaurant.name = edit.name;
+      restaurant.name_en = edit.name_en;
+      restaurant.phone = edit.phone;
+      restaurant.location = edit.location;
+      restaurant.category = edit.category;
+      restaurant.description = edit.description;
+      return restaurant.save();
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
     .catch((error) => console.log(error));
 });
 
