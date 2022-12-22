b00ca2613041fc82980cde94ffe29bdb0aaac4b2
Isis Lin <qweb32114@gmail.com>
2022-12-22 14:00:43 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const session = require("express-session");
 
 const router = require("./routes");
 require("./config/mongoose");
@@ -14,6 +15,14 @@ app.set("view engine", "hbs");
 
 // 宣告express使用靜態css文件
 app.use(express.static("./public"));
+
+app.use(
+  session({
+    secret: "Thisismykey",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
